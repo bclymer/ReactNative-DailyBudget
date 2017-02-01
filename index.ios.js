e21d30db1bbdd1e5ffcc80664d0f22d647c43e95
Brian Clymer <crdnilfan@gmail.com>
2017-01-31 21:12:00 -0600

    Add realm yay.

--- index.ios.js
+++ index.ios.js
@@ -1,24 +1,54 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { AppRegistry, Text, TextInput, View, StatusBar } from 'react-native';
+import { ListView } from 'realm/react-native';
 
-class Greeting extends Component {
-  render() {
-    return (
-      <Text>Hello {this.props.name}!</Text>
-    );
+const Realm = require('realm');
+
+const BudgetSchema = {
+  name: 'Budget',
+  primaryKey: 'id',
+  properties: {
+    id:  {type: 'int', default: 0},
+    name: {type: 'string', optional: true},
+    amountPerPeriod: {type: 'double', default: 0},
+    periodLengthInDays: {type: 'int', default: 0},
+    cachedValue: {type: 'double', default: 0},
+    //cachedDate: {type: 'date', default: Date()},
+    transactions: {type: 'list', objectType: 'Transaction'}
   }
+};
+const TransactionSchema = {
+  name: 'Transaction',
+  primaryKey: 'id',
+  properties: {
+    id:  {type: 'int', default: 0},
+    cachedDate: {type: 'date', default: Date(0)},
+    amount: {type: 'double', default: 0},
+    vendor: {type: 'string', optional: true},
+    budget: {type: 'Budget'},
+  }
+};
+
+let realm = new Realm({schema: [BudgetSchema, TransactionSchema]});
+
+class DailyBudget extends Component {
+ render() {
+
+   realm.write(() => {
+     realm.create('Budget', {
+       id: 3
+     });
+   });
+
+   return (
+     <View style={{alignItems: 'center'}}>
+       <StatusBar hidden />
+       <Text>
+         Count of Transactions in Realm: {realm.objects('Budget').length}
+       </Text>
+     </View>
+   );
+ }
 }
 
-class LotsOfGreetings extends Component {
-  render() {
-    return (
-      <View style={{alignItems: 'center'}}>
-        <Greeting name='Rexxar' />
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
-      </View>
-    );
-  }
-}
-
-AppRegistry.registerComponent('DailyBudget', () => LotsOfGreetings);
+AppRegistry.registerComponent('DailyBudget', () => DailyBudget);
