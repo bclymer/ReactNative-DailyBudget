417fa42c7df9826d2591d91cb48291147e43a5a6
Brian Clymer <crdnilfan@gmail.com>
2017-02-01 19:41:38 -0600

    Budget list item is a clickable component

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View, StatusBar } from 'react-native';
+import { AppRegistry, Text, View, StatusBar, TouchableOpacity } from 'react-native';
 import { ListView } from 'realm/react-native';
 import ActionButton from 'react-native-action-button';
 
@@ -9,33 +9,51 @@ const BudgetSchema = {
   name: 'Budget',
   primaryKey: 'id',
   properties: {
-    id:  {type: 'int', default: 0},
-    name: {type: 'string', optional: true},
-    amountPerPeriod: {type: 'double', default: 0},
-    periodLengthInDays: {type: 'int', default: 0},
-    cachedValue: {type: 'double', default: 0},
+    id: { type: 'int', default: 0 },
+    name: { type: 'string', optional: true },
+    amountPerPeriod: { type: 'double', default: 0 },
+    periodLengthInDays: { type: 'int', default: 0 },
+    cachedValue: { type: 'double', default: 0 },
     //cachedDate: {type: 'date', default: Date()},
-    transactions: {type: 'list', objectType: 'Transaction'}
+    transactions: { type: 'list', objectType: 'Transaction' }
   }
 };
 const TransactionSchema = {
   name: 'Transaction',
   primaryKey: 'id',
   properties: {
-    id:  {type: 'int', default: 0},
-    cachedDate: {type: 'date', default: Date(0)},
-    amount: {type: 'double', default: 0},
-    vendor: {type: 'string', optional: true},
-    budget: {type: 'Budget'},
+    id: { type: 'int', default: 0 },
+    cachedDate: { type: 'date', default: Date(0) },
+    amount: { type: 'double', default: 0 },
+    vendor: { type: 'string', optional: true },
+    budget: { type: 'Budget' },
   }
 };
 
-let realm = new Realm({schema: [BudgetSchema, TransactionSchema]});
+let realm = new Realm({ schema: [BudgetSchema, TransactionSchema] });
+
+class BudgetListItem extends Component {
+
+  openBudget() {
+    console.log("OPENING THE BUDGET " + this.props.budget.name);
+  }
+
+  render() {
+    return (
+      <TouchableOpacity onPress={this.openBudget.bind(this)}>
+        <View style={{ flex: 1, backgroundColor: '#f3f3f3', height: 40, justifyContent: 'center' }}>
+          <Text style={{ left: 20 }}>{this.props.budget.name}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
+}
 
 class DailyBudget extends Component {
   constructor(props) {
     super(props);
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     this.state = {
       dataSource: ds.cloneWithRows(ds),
       items: realm.objects('Budget')
@@ -49,29 +67,26 @@ class DailyBudget extends Component {
     });
   }
 
- render() {
+  render() {
 
-   return (
-     <View style={{flex:1, backgroundColor: '#f3f3f3'}}>
-     <ListView
+    return (
+      <View style={{ flex: 1, backgroundColor: '#f3f3f3' }}>
+        <StatusBar hidden />
+        <ListView
           dataSource={this.state.dataSource}
-          renderRow={(rowData) => <Text>{rowData.name}</Text>}
-      />
-      <ActionButton buttonColor="rgba(231,76,60,1)" onPress={() => {
-        realm.write(() => {
-          realm.create('Budget', {
-            id: (realm.objects('Budget').length + 1),
-            name: Math.random().toString(36).substr(2, 5)
+          renderRow={(rowData) => <BudgetListItem budget={rowData} />}
+          />
+        <ActionButton buttonColor="rgba(231,76,60,1)" onPress={() => {
+          realm.write(() => {
+            realm.create('Budget', {
+              id: (realm.objects('Budget').length + 1),
+              name: Math.random().toString(36).substr(2, 5)
+            });
           });
-        });
-      }} />
-       <StatusBar hidden />
-       <Text>
-         Count of Transactions in Realm: {this.state.items.length}
-       </Text>
-     </View>
+        } } />
+      </View>
    );
- }
+  }
 }
 
 AppRegistry.registerComponent('DailyBudget', () => DailyBudget);
